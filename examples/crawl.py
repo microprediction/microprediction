09b079a11d160e51e7097cdf95d2641d6d858f1b
microprediction <info@3za.org>
2020-03-18 14:32:50 -0400

    ah

--- examples/crawl.py
+++ examples/crawl.py
@@ -1,6 +1,6 @@
 from microprediction import MicroCrawler
 
-write_key = "fa813cd1f1bf6cec3ef74efeaedb01f4"   # <--- Replace with your own or it won't work
+write_key = "fa813cd1f1bf6cec3ef74efeaedb01f4"   # <--- Click on http://www.muid.org/create/ to get one
 crawler   = MicroCrawler(write_key=write_key)
 crawler.run()
 
