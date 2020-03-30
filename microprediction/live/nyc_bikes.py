d0d8f8a0fe36a35a4114d784940c615cba8597ca
ezl-13 <ericzlou@gmail.com>
2020-03-30 16:55:06 -0400

    Final set of hospitals

--- microprediction/live/nyc_bikes.py
+++ microprediction/live/nyc_bikes.py
@@ -6,13 +6,15 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from microprediction import MicroWriter
 from microprediction.config_private import TRAFFIC_WRITE_KEY
 
-
 # Number of bikes available near large Manhattan hospitals
-# longitude and latitude of rectangles within 200 feet of hospital
+# longitude and latitude of rectangles within 500 feet of hospital
+# citibike map: https://member.citibikenyc.com/map/
 COORDS = [
     ((40.787880, -73.955862), (40.792001, -73.948389)),     # Mount Sinai - station_ids: 3345, 3299, 3363
-    ((40.763573, -73.959175), (40.766666, -73.950179)),     # New York Presbytarian - station_ids: 3156, 3141
+    ((40.763573, -73.959175), (40.766666, -73.950179)),     # New York Presbytarian Upper East Side - station_ids: 3156, 3141
     ((40.735625, -73.979437), (40.743511, -73.970913)),     # Bellevue and NYU Langone - station_ids: 545, 3687, 174, 528, 536, 2012
+    ((40.709229, -74.008624), (40.712033, -74.002787)),     # New York Presbytarian Lower Manhattan - station_ids: 224, 316
+    ((40.783116, -73.949118), (40.787085, -73.939771)),     # Metropolitan Hospital Center - station_ids: 3309, 3318, 3338, 3351
 ]
 
 def get_station_ids():
@@ -64,7 +66,8 @@ print("Initial value is " + str(initial_value), flush=True)
 def poll_and_send():
     """ Create stream of citibike count """
     value = hospital_bikes_available(station_ids)
-    res = mw.set(name=NAME,value=float(value))
+    # res = mw.set(name=NAME,value=float(value))
+    res = 0
     pprint({'citibikes':value,"res":res})
     print('',flush=True)
 
