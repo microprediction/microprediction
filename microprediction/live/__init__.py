6fe957a9bc620aecf92fb4ea2b74432e1dd3ff95
ezl-13 <ericzlou@gmail.com>
2020-04-03 15:52:37 -0400

    BART does not publish in off-hours

--- microprediction/live/__init__.py
+++ microprediction/live/__init__.py
@@ -1,3 +1,2 @@
 from microprediction.live.nyc_traffic import bronx_speed, verrazano_speed
-from microprediction.live.nyc_bikes import madison_bikes_available
 
--- microprediction/live/bart_delays.py
+++ microprediction/live/bart_delays.py
@@ -1,5 +1,6 @@
 import requests
-import datetime
+from datetime import datetime
+import pytz
 from pprint import  pprint
 from apscheduler.schedulers.blocking import BlockingScheduler
 
@@ -19,8 +20,9 @@ def fetch_live_data(station):
             for destination in station["etd"]:
                 if len(destination["estimate"]) is not 0:
                     total_delay += int(destination["estimate"][0]["delay"])
-                lines += 1
-        return float(total_delay) / lines
+                    lines += 1
+        # fail-safe for wide BART shutdowns. On average ~180 lines
+        return float(total_delay) / lines if lines > 15 else None
     else:
         return None
 
@@ -34,18 +36,44 @@ NAME = 'bart_delays.json'
 initial_value = all_stations_delay()
 print("Initial value is " + str(initial_value) + " seconds", flush=True)
 
+
 try:
     mw = MicroWriter(write_key=TRAFFIC_WRITE_KEY)
 except:
     raise Exception("You need to set the write key for this example to work")
 
+def record_json(pst_now):
+    import json
+    r = requests.get("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=ALL&key="+BART_KEY+"&json=y")
+    filename = "record_" + pst_now.strftime("%H:%M:%S") + ".json"
+    with open(filename, 'w') as f:
+        json.dump(r.json(), f)
+
 
 def poll_and_send():
     """ Create stream of average delay in seconds """
+
+    # avoid publishing during BART's off-hours
+    # NOTE: these are COVID-specific hours; adjust later
+    # weekday hours: 5am-9pm
+    # weekend hours: 8am-9pm
+    utc_now = pytz.utc.localize(datetime.utcnow())
+    pst_now = utc_now.astimezone(pytz.timezone("America/Los_Angeles"))
+    if pst_now.weekday() <= 4 and (pst_now.hour < 5 or pst_now.hour >= 21) \
+    or pst_now.weekday() >= 5 and (pst_now.hour < 8 or pst_now.hour >= 21):
+        print("{}: Off-Hours".format(pst_now.strftime("%H:%M:%S")))
+        record_json(pst_now)
+        return
+
     value = all_stations_delay()
-    res = mw.set(name=NAME,value=float(value))
-    pprint({'average delay':value,"res":res})
-    print('',flush=True)
+
+    if value is None:
+        print("{}: <= 15 Lines but On-Hours".format(pst_now.strftime("%H:%M:%S")))
+        record_json(pst_now)
+    else:
+        res = mw.set(name=NAME,value=float(value))
+        pprint({'PST time':pst_now.strftime("%H:%M:%S"),'average delay':value,"res":res})
+        print('',flush=True)
 
 def run():
     print('Starting scheduler',flush=True)
