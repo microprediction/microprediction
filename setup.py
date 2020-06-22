39138c0c0d28d067adeb7f63a1090c703b4e3662
microprediction <info@3za.org>
2020-06-22 07:43:24 -0400

    version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="microprediction",
-    version="0.7.0",
+    version="0.7.1",
     description="Client for www.microprediction.org",
     long_description=README,
     long_description_content_type="text/markdown",
