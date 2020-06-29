2ca85732ca07a11d4194a3142d9666ffcf98b51f
microprediction <info@3za.org>
2020-06-29 09:03:03 -0400

    less mining

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="microprediction",
-    version="0.7.2",
+    version="0.7.4",
     description="Client for www.microprediction.org",
     long_description=README,
     long_description_content_type="text/markdown",
