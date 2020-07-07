3d592359f6fb8edc5bc60e2fb2330a611bb36657
microprediction <info@3za.org>
2020-07-07 15:21:28 -0400

    delays

--- tests/test_constructors.py
+++ tests/test_constructors.py
@@ -4,16 +4,16 @@ from microprediction.conventions import create_key
 
 def test_delays():
     mc = MicroConventions()
-    assert len(mc.DELAYS)==3
+    assert len(mc.DELAYS)==4
 
 def test_delays_1():
     mc = MicroReader()
-    assert len(mc.DELAYS)==3
+    assert len(mc.DELAYS)==4
 
 def test_delays_2():
     mc = MicroWriter(write_key=create_key(difficulty=7))
-    assert len(mc.DELAYS)==3
+    assert len(mc.DELAYS)==4
 
 def test_delays_3():
     mc = MicroCrawler(write_key=create_key(difficulty=7))
-    assert len(mc.DELAYS)==3
+    assert len(mc.DELAYS)==4
