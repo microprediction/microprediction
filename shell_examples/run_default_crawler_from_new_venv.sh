634c75552f2f9d2a13db26ae43cb3e9a61c99e10
Peter Cotton <pcotton@intechinvestments.com>
2020-08-06 08:26:58 -0400

    script adjustment

--- shell_examples/run_default_crawler_from_new_venv.sh
+++ shell_examples/run_default_crawler_from_new_venv.sh
@@ -1,6 +1,10 @@
+set -e
+echo "This script will run DefaultCrawler from a new virtual env"
 mkdir crawling_working_dir
 cd crawling_working_dir
 python3 -m venv crawling
 source crawling/bin/activate
+pip install --upgrade pip
 pip install --upgrade microprediction
+echo "Be sure to grab the write_key and paste it into your dashboard at https://www.microprediction.org"
 python3 -c "from microprediction import DefaultCrawler;crawler = DefaultCrawler(); crawler.run()"
