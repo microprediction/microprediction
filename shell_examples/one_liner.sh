fe4b1621b8daea99a92443691a4592333b3bea81
Peter Cotton <pcotton@intechinvestments.com>
2020-08-06 08:19:22 -0400

    fix path in script

--- shell_examples/one_liner.sh
+++ shell_examples/one_liner.sh
@@ -1,1 +1,1 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/microprediction/muid/master/shell_examples/run_default_crawler_from_new_venv.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/microprediction/microprediction/master/shell_examples/run_default_crawler_from_new_venv.sh)"
