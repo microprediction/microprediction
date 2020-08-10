87d66a199e4ed7b91a43c76aa69b1b7a7de0a091
Peter Cotton <pcotton@intechinvestments.com>
2020-08-10 10:55:59 -0400

    moment test for skewnorm

--- tests/test_skewdist.py
+++ tests/test_skewdist.py
@@ -1,15 +1,23 @@
 from microprediction.univariate.skewdist import SkewDist
 from microconventions.stats_conventions import StatsConventions
 import numpy as np
+from scipy.stats import skewnorm
+import random
 
 
 def test_skewdist():
-    sd = SkewDist(num_predictions=250)
-    xs = np.random.randn(100)
+    sd = SkewDist(num_predictions=500)
+    a = random.choice([2., 3., 4.])
+    xs = skewnorm.rvs(a, size=1000)
+
     for x in xs:
         sd.update(value=x)
 
-    percentiles =  [0.25, 0.5, 0.75]
-    skew_quantiles = [ sd.inv_cdf(p) for p in percentiles ]
-    norm_quantiles = [ StatsConventions.norminv(p) for p in percentiles ]
-    assert abs(skew_quantiles[1])<0.50
+    percentiles = [0.25, 0.5, 0.75]
+    skew_quantiles = [sd.inv_cdf(p) for p in percentiles]
+    actual_quantiles = [np.quantile(xs, q) for q in percentiles]
+    assert all(abs(z1 - z2) < 0.5 for z1, z2 in zip(skew_quantiles, actual_quantiles))
+
+    sd_stats = [sd.mean, sd.var(), sd.skewness(), sd.kurtosis()]
+    moment_comparison = list(zip(sd_stats, skewnorm.stats(a, moments='mvsk')))
+    pass
