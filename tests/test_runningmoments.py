05a595d46fbf988c9cb24c7e69a3b0983852f9a0
Peter Cotton <pcotton@intechinvestments.com>
2020-08-10 10:23:32 -0400

    still removing creme

--- tests/test_runningmoments.py
+++ tests/test_runningmoments.py
@@ -1,29 +1,46 @@
 from microprediction.univariate.runningmoments import RunningVariance, RunningKurtosis
 import numpy as np
 from statistics import variance, pvariance
-from creme.preprocessing import StandardScaler
 from scipy.stats import kurtosis, skew
 
 
 # Check against creme
 
+
+def test_normal_against_creme():
+    try:
+        from creme.preprocessing import StandardScaler
+        xs = list(np.random.randn(100))
+        machine = RunningVariance()
+        scalar = StandardScaler()
+        for x in xs:
+            machine.update(value=x)
+            scalar.fit_one({'x': x})
+        var1 = np.var(xs)
+        var2 = machine.var()
+        var3 = variance(xs)
+        var4 = pvariance(xs)
+        var5 = scalar.vars['x']
+        var6 = machine.pvar()
+        print([var1, var2, var3, var4, var5, var6])
+        assert abs(var2 - var3) < 0.0001
+        assert abs(var5 - var1) < 0.0001
+        assert abs(var5 - var6) < 0.0001
+    except ImportError:
+        pass
+
 def test_normal():
     xs = list(np.random.randn(100))
     machine = RunningVariance()
-    scalar = StandardScaler()
     for x in xs:
         machine.update(value=x)
-        scalar.fit_one({'x': x})
     var1 = np.var(xs)
     var2 = machine.var()
     var3 = variance(xs)
     var4 = pvariance(xs)
-    var5 = scalar.vars['x']
     var6 = machine.pvar()
-    print([var1, var2, var3, var4, var5, var6])
+    print([var1, var2, var3, var4, var6])
     assert abs(var2 - var3) < 0.0001
-    assert abs(var5 - var1) < 0.0001
-    assert abs(var5 - var6) < 0.0001
 
 
 def test_kurtosis():
