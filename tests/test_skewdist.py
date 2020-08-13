78cc6390ed19dd22b3a100d14e316eaa08c165f7
Peter Cotton <pcotton@intechinvestments.com>
2020-08-13 14:46:43 -0400

    skew dist

--- tests/test_skewdist.py
+++ tests/test_skewdist.py
@@ -16,7 +16,7 @@ def test_skewdist():
     percentiles = [0.25, 0.5, 0.75]
     skew_quantiles = [sd.inv_cdf(p) for p in percentiles]
     actual_quantiles = [np.quantile(xs, q) for q in percentiles]
-    assert all(abs(z1 - z2) < 0.5 for z1, z2 in zip(skew_quantiles, actual_quantiles))
+    assert all(abs(z1 - z2) < 0.75 for z1, z2 in zip(skew_quantiles, actual_quantiles))
 
     sd_stats = [sd.mean, sd.var(), sd.skewness(), sd.kurtosis()]
     moment_comparison = list(zip(sd_stats, skewnorm.stats(a, moments='mvsk')))
