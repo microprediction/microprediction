ca9f558e2a8185777d5b288f9c3e92ee512c645e
Peter Cotton <pcotton@intechinvestments.com>
2020-08-13 21:11:07 -0400

    tests fixed

---
+++ tests/test_submit_against_site.py
@@ -0,0 +1,11 @@
+from microprediction.writer import MicroWriter
+from microprediction.set_config import MICRO_TEST_CONFIG
+
+BASE_URLS = MICRO_TEST_CONFIG['BASE_URLS']
+
+def test_die_submit():
+    for base_url in BASE_URLS:
+        mw = MicroWriter(write_key=MICRO_TEST_CONFIG['TEST_WRITE_KEY'],base_url=base_url)
+        die_values = [-2.5,-1.5,-0.5,0.5,1.5,2.5]*37+[-2.5,-1.5,2.5]
+        for delay in mw.DELAYS:
+            perf = mw.submit(name='die.json',values=die_values,delay=delay)
