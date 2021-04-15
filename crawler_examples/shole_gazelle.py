7c599161ad8a77bdde8eafffddf6309cd296844e
Peter Cotton <peter.cotton@microprediction.com>
2021-04-14 21:41:24 -0400

    gazelle example

--- crawler_examples/shole_gazelle.py
+++ crawler_examples/shole_gazelle.py
@@ -1,21 +1,14 @@
 from microprediction.config_private import SHOLE_GAZELLE
 from microprediction.streamskater import StreamSkater
 
-# Example of a "skater" that uses the timemachines package for point estimates
-
-
-try:
-    from timemachines.skaters.simple import thinking_slow_and_fast
-    using_tm = True
-except ImportError:
-    using_tm = False
-    thinking_slow_and_fast = None
-
+# Example of a "skater" that uses the TimeMachines package for point estimates
 
 if __name__=='__main__':
-    if using_tm:
-        skater = StreamSkater(write_key=SHOLE_GAZELLE,f=thinking_slow_and_fast,use_std=False)
-        skater.set_repository('https://github.com/microprediction/microprediction/blob/master/crawler_examples/shole_gazelle.py')
+    try:
+        from timemachines.skaters.simple import thinking_slow_and_fast
+        skater = StreamSkater(write_key=SHOLE_GAZELLE, f=thinking_slow_and_fast, use_std=False)
+        skater.set_repository(
+            'https://github.com/microprediction/microprediction/blob/master/crawler_examples/shole_gazelle.py')
         skater.run()
-    else:
+    except ImportError:
         print('pip install timemachines')
