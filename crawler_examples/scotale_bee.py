5f29a78d6f9d6da5a2c5e5d82476bdd5bc8143eb
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-07-12 21:24:23 -0400

    THINKING_FAST_AND_SLOW

---
+++ crawler_examples/scotale_bee.py
@@ -0,0 +1,16 @@
+from microprediction.config_private import SCOTALE_BEE
+from microprediction.streamskater import StreamSkater
+
+# Example of a "skater" that uses the TimeMachines package for point estimates
+# This crawls www.microprediction.org, as explained by the helper site www.microprediction.com
+
+try:
+    from timemachines.skaters.simple.thinking import thinking_fast_and_slow
+except ImportError:
+    print('pip install timemachines')
+
+if __name__=='__main__':
+    skater = StreamSkater(write_key=SCOTALE_BEE, f=slow_aggressive_ema_ensemble, use_std=False, max_active=1000)
+    skater.set_repository(
+        'https://github.com/microprediction/microprediction/blob/master/crawler_examples/scotale_bee.py')
+    skater.run()
