6bbe1369714169c8d875f21676cb671f9c575e6e
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-07-12 20:56:30 -0400

    switched to TSA

--- crawler_examples/smeech_clam.py
+++ crawler_examples/smeech_clam.py
@@ -1,10 +1,16 @@
-from microprediction import SequentialStreamCrawler
 from microprediction.config_private import SMEECH_CLAM
+from microprediction.streamskater import StreamSkater
 
-# New video tutorials are available at https://www.microprediction.com/python-1 to help you
-# get started running crawlers at www.microprediction.com
+# Example of a "skater" that uses the TimeMachines package for point estimates
+# This crawls www.microprediction.org, as explained by the helper site www.microprediction.com
 
-if __name__ == '__main__':
-    crawler = SequentialStreamCrawler(write_key=SMEECH_CLAM, max_active=50, stop_loss=5)
-    crawler.set_repository('https://github.com/microprediction/microprediction/blob/master/crawler_examples/smeech_clam.py')
-    crawler.run()
+try:
+    from timemachines.skaters.tsa.tsaconstant import tsa_p3_d0_q1
+except ImportError:
+    print('pip install timemachines')
+
+if __name__=='__main__':
+    skater = StreamSkater(write_key=SMEECH_CLAM, f=tsa_p3_d0_q1, use_std=False, max_active=1000)
+    skater.set_repository(
+        'https://github.com/microprediction/microprediction/blob/master/crawler_examples/smeech_clam.py')
+    skater.run()
