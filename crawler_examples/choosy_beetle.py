a880619880da5d1549bed4b4cef817806a3b1c0b
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-07-12 20:03:38 -0400

    Create choosy_beetle.py

---
+++ crawler_examples/choosy_beetle.py
@@ -0,0 +1,16 @@
+from microprediction.config_private import DATABLE_LLAMA
+from microprediction.streamskater import StreamSkater
+
+# Example of a "skater" that uses the TimeMachines package for point estimates
+# This crawls www.microprediction.org, as explained by the helper site www.microprediction.com
+
+try:
+    from timemachines.skaters.tsa.tsaconstant import tsa_p2_d0_q1
+except ImportError:
+    print('pip install timemachines')
+
+if __name__=='__main__':
+    skater = StreamSkater(write_key=DATABLE_LLAMA, f=tsa_p2_d0_q1, use_std=True, max_active=1000)
+    skater.set_repository(
+        'https://github.com/microprediction/microprediction/blob/master/crawler_examples/datable_llama.py')
+    skater.run()
