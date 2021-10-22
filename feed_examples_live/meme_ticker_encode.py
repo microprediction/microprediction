2e6ccc28e037d643603c678ec6b1423c53d8ff2a
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-10-21 21:01:01 -0400

    Create meme_ticker_encode.py

---
+++ feed_examples_live/meme_ticker_encode.py
@@ -0,0 +1,13 @@
+# For those looking at the meme of the day stocks
+# Complain to Graham 
+
+from re import findall
+from functools import reduce
+
+def decode(encoded):
+    """Decode an integer to a ticker."""
+    return str(reduce(lambda x,y:x+y,map(lambda x:chr(int(x)),findall(r"[0-9][0-9]",str(int(encoded))))))
+
+def encode(decoded):
+    """Encode a ticker as an integer."""
+    return int(reduce(lambda x,y:x+y,map(lambda x:str(ord(x)),list(decoded))))
