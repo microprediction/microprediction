4a74c9ff9099e12ba20deb6400738c3f6a4851c2
Peter Cotton <peter.cotton@microprediction.com>
2021-11-30 17:20:49 -0500

    notebook

--- submission_examples_die/enter_die_contest_one_off.py
+++ submission_examples_die/enter_die_contest_one_off.py
@@ -1,16 +1,17 @@
 from microprediction import new_key, MicroWriter
-from pprint import pprint
+import random
 
-# The absolute quickish way to enter a contest ... just run this script once and you're good
+# Get started ... just run this script once and you're good
 # Video tutorials are available at https://www.microprediction.com/python-1
 
 if __name__ == '__main__':
-    write_key = new_key(difficulty=9)
+    print("Thanks for participating. First I need to create your identity. Be patient.")
+    write_key = new_key(difficulty=10)
     mw = MicroWriter(write_key=write_key)
     die_rolls = [k - 2.5 for k in range(6)] * 50
-    values = die_rolls[:mw.num_predictions]
-    pprint(mw.submit(name='die.json', values=values))
-    print('https://api.microprediction.org/home/' + write_key)
-
+    random.shuffle(die_rolls)
+    values = sorted(die_rolls[:mw.num_predictions])
+    for delay in mw.DELAYS:
+        mw.submit(name='die.json', values=values, delay=delay)
     print('Now go to https://www.microprediction.org and enter '+write_key+' into the dashboard ')
 
