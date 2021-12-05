74146402cedab15500db11eebda8bd10ce966782
Peter Cotton <peter.cotton@microprediction.com>
2021-12-05 11:01:44 -0500

    meme stock again

---
+++ microprediction/bespoke/crypto/cryptoconventions.py
@@ -0,0 +1,5 @@
+
+
+C5_COINS = 'bitcoin,ethereum,tether,cardano,solana'
+NAMES = [ 'c5_'+name+'.json' for name in C5_COINS.split(',') ]
+
--- tests/bespoke/test_meme_stock.py
+++ tests/bespoke/test_meme_stock.py
@@ -1,6 +1,17 @@
-from microprediction.bespoke.meme_stock import encode_meme_stock, decode_meme_stock
+from microprediction.bespoke.meme_stock import encode_meme_stock_old, decode_meme_stock_old, encode_meme_stock, decode_meme_stock
 
 
 def test_encodings():
-    assert (encode_meme_stock('GME') == 717769)
-    assert (encode_meme_stock('TSLA') == 84837665)
+    assert (encode_meme_stock_old('GME') == 717769)
+    assert (encode_meme_stock_old('TSLA') == 84837665)
+
+
+def test_round_trip():
+    for ticker in ['GME','ABAB','TSLA','LONGTICKER']:
+        y = encode_meme_stock(ticker)
+        assert ticker==decode_meme_stock(y)
+
+
+if __name__=='__main__':
+    print(test_round_trip())
+    print((encode_meme_stock('CC')-encode_meme_stock_old('BB'))/1e-4)
