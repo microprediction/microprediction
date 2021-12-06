703f585d6210f74818b2038ec4b906e37b938db4
Graham Giller <graham@gillerinvestments.com>
2021-12-05 19:33:11 -0500

    Update meme_stock.py

--- microprediction/bespoke/meme_stock.py
+++ microprediction/bespoke/meme_stock.py
@@ -10,17 +10,26 @@ def decode_meme_stock_old(encoded):
         lambda x:chr(int(x)),findall(r"[0-9][0-9]",
         str(int(encoded))))))
 
-
 def encode_meme_stock_old(decoded):
     """Encode a ticker as an integer."""
     return int(reduce(lambda x,y:x+y,map(
          lambda x:str(ord(x)),list(decoded))))
 
+# Versions that permit an upper limit to be placed on the encoded value to deal with precision/rounding issues.
+def decode_meme_stock(encoded,epsilon=0.1e0,scale=1e2):
+    """Decode a float to a ticker. Values with parts more than epsilon will be increased by scale until this is no longer true."""
+    f=float(encoded)
+    
+    while abs(f-float(int(f)))>epsilon:
+        f*=scale
+    
+    return str(reduce(lambda x,y:x+y,map(lambda x:chr(int(x)),findall(r"[0-9][0-9]",str(int(f))))))
 
-def decode_meme_stock(encoded):
-    return decode_meme_stock_old(100*encoded)
-
-
-def encode_meme_stock(decoded):
-    encoded = encode_meme_stock_old(decoded)
-    return encoded/100.0
+def encode_meme_stock(decoded,ceiling=1e7,scale=1e2):
+    """Encode a ticker as a float. Values in excess of ceiling will be reduced by scale until they are not in excess."""
+    f=int(reduce(lambda x,y:x+y,map(lambda x:str(ord(x)),list(decoded))))
+    
+    while f>ceiling:
+        f/=scale
+        
+    return f
