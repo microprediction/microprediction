2ceb8db551d867076aadaa60dd044d5fb05cbcae
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-12-16 19:02:43 -0500

    Update cystose_eel.py

--- submission_examples_meme_stocks/cystose_eel.py
+++ submission_examples_meme_stocks/cystose_eel.py
@@ -5,12 +5,19 @@ from pprint import pprint
 import numpy as np
 import time
 
+# This part is only needed for interpretation / logging
+
+verbose = False
+stop_loss = 25
+
 def fix_lagged(x):
     while x>1e7:
         x = x/100.0
     return x
 
 # Implements a quasi-transition model for the Meme stock of the hour
+
+NAME = 'meme-stock-of-the-hour.json'
 
 def values_from_lagged(lagged_values:[float], num_predictions=225)->[float]:
     """
@@ -36,29 +43,36 @@ def values_from_lagged(lagged_values:[float], num_predictions=225)->[float]:
     return sorted(values[:num_predictions])
 
 
-
-def submit_predictions():
+def submit_predictions(writer,horizons_to_avoid):
     """
         Example of creating and submitting predictions for the next meme stock
     """
-    mw = MicroWriter(write_key=CYSTOSE_EEL)
-    mw.set_repository('https://github.com/microprediction/microprediction/blob/master/crawler_alternatives_meme_stock/cystose_eel.py')
-    names = [ nm for nm in mw.get_stream_names() if 'meme' in nm and not '~' in nm ]
+    names = [ nm for nm in mw.get_stream_names() if not '~' in nm ]
     for name in names:
         lagged_values = [ fix_lagged(x) for x in mw.get_lagged_values(name=name) ]
         if len(lagged_values)>225:
             values = values_from_lagged(lagged_values=lagged_values, num_predictions=mw.num_predictions)
             for delay in mw.DELAYS:
-                res = mw.submit(name=name,values=values,delay=delay)
-                pprint(res)
-                time.sleep(0.5)
+                horizon = mw.horizon_name(name=name,delay=delay)
+                if horizon not in horizons_to_avoid:
+                    try:
+                        mw.submit(name=name,values=values,delay=delay)
+                    except:
+                        print('failed to submit')
+                        print(len(set(values)))
+                    time.sleep(0.5)
 
 
 if __name__=='__main__':
+    mw = MicroWriter(write_key=CYSTOSE_EEL)
+    mw.set_repository('https://github.com/microprediction/microprediction/blob/master/submission_examples_transition/cystose_eel.py')
+    performance = mw.get_performance()
+    horizons_to_avoid = [h for h,b in performance.items() if b<-abs(stop_loss) ]
+    mw.cancel_worst_active(stop_loss=stop_loss)
+
     # Assumes this script will be run hourly
     for _ in range(7):
-        submit_predictions()
+        submit_predictions(writer=mw,horizons_to_avoid=horizons_to_avoid)
         time.sleep(60*8)
-
 
 
