e8620d2b33af1fc1e6cf4f3975519f7e22299fb7
Peter Cotton <peter.cotton@microprediction.com>
2022-03-17 12:09:21 -0400

    live feed

--- microprediction/live/iex.py
+++ microprediction/live/iex.py
@@ -29,12 +29,14 @@ def iex_weighted_mid(d, expon=-1.5):
     m1 = d['iexBidSize']
     m2 = d['iexAskSize']
     if (x1 != 0) and (x2 != 0) and (m1 != 0) and (m2 != 0) and (x2 > x1):
-        return weighted_mid(x1=x1, x2=x2, m1=m1, m2=m2)
+        wm = weighted_mid(x1=x1, x2=x2, m1=m1, m2=m2)
+        print({'x1': x1, 'm1': m1, 'x2': x2, 'm2': m2, 'wm': wm})
+        return wm
     else:
         return d['latestPrice']
 
 
-def iex_realtime_prices(tickers, api_key: str, use_free_api=False) -> [float]:
+def iex_weighted_mid_prices(tickers, api_key: str, use_free_api=False) -> [float]:
     """
         Asynchronously grab multiple stock prices
     """
@@ -47,9 +49,21 @@ def iex_realtime_prices(tickers, api_key: str, use_free_api=False) -> [float]:
     return mids
 
 
+def iex_latest_prices(tickers, api_key:str)->[float]:
+    """
+        Asynchronously grab multiple stock prices
+    """
+    PAID_URL = 'https://cloud.iexapis.com/stable/stock/TICKER/quote?token=YOUR_TOKEN_HERE'
+    price_urls = [PAID_URL.replace('YOUR_TOKEN_HERE', api_key).replace('TICKER', ticker) for ticker in tickers]
+    d = mgetjson(urls=price_urls)
+    mids = [ di['latestPrice'] for di in d ]
+    return mids
+
+
 if __name__ == '__main__':
     from microprediction.config_private import IEX_KEY
-
-    print(iex_realtime_prices(tickers=['aapl', 'googl'], api_key=IEX_KEY))
+    for _ in range(200):
+        time.sleep(1)
+        print(iex_latest_prices(tickers=['googl'], api_key=IEX_KEY))
 
 
--- stream_examples_faang/faang.py
+++ stream_examples_faang/faang.py
@@ -1,12 +1,11 @@
 from microprediction.config_private import LEGLESS_OCELOT as WRITE_KEY
 from microprediction.config_private import IEX_KEY
 from microprediction.polling import MultiChangePoll
-from microprediction.live.iex import iex_realtime_prices
+from microprediction.live.iex import iex_latest_prices
 import math
 import numpy as np
 
 
-# "Show me the money"
 # Creates a collection of streams related to stocks and portfolios of the same
 
 
@@ -36,7 +35,7 @@ GNAAF = [ name_from_comb(c) for c in comb]
 
 
 def scaled_log_faang():
-    sp = iex_realtime_prices(tickers=FAANG, api_key=IEX_KEY)
+    sp = iex_latest_prices(tickers=FAANG, api_key=IEX_KEY)
     return [1000 * math.log(v) for v in sp]
 
 
@@ -44,9 +43,10 @@ def scaled_portfolio_return(changes, w:[float]):
     """
        Compute scaled portfolio log changes from scaled asset log changes
     """
-    w = np.array(w)/np.sum(w)
-    f = np.exp( changes/1000.)
-    change = 1000*math.log( np.inner(w,f) )
+    w = np.array(w)/np.sum(w)   # Ensure weights normalized
+    f = np.exp( changes/1000.)  # Multiplicative increase in wealth
+    w_post = np.inner( w, f)    # Posterior unit portfolio wealth
+    change = 1000*math.log( w_post )
     return change
 
 
@@ -58,7 +58,7 @@ if __name__=='__main__':
     faang_names = [ 'faang_'+ticker+'.json' for ticker in TICKERS ]
     names = faang_names + GNAAF
 
-    mcp = MultiChangePoll(write_key=WRITE_KEY, names = names, interval=1, func=scaled_log_faang, with_copulas=False,
+    mcp = MultiChangePoll(write_key=WRITE_KEY, names = names, interval=5, func=scaled_log_faang, with_copulas=False,
                           change_func=change_func)
     mcp.set_repository('https://github.com/microprediction/microprediction/tree/master/stream_examples_stocks')
     mcp.run()
