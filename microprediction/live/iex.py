400c42ff6420bb4a6978b59996d95b18b743e6d0
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-03-17 10:12:42 -0400

    Update iex.py

--- microprediction/live/iex.py
+++ microprediction/live/iex.py
@@ -1,19 +1,55 @@
 from getjson import mgetjson
-
+from scipy.optimize import newton
+import time
 
 # IEX stock prices
 # You can get your api_key at https://iexcloud.io/
+# See https://iexcloud.io/docs/api/#quote
+
+def force(x, x1, x2, m1, m2, expon=2):
+    return m1 / (abs(x - x1) ** expon) - m2 / (abs(x - x2) ** 2)
 
 
-def iex_realtime_prices(tickers, api_key:str, use_free_api=False )->[float]:
+def weighted_mid(x1, x2, m1, m2, expon=2):
+    if x1 >= x2:
+        return x1
+    else:
+        x_mid = 0.5*x1+0.5*x2
+        x_weighted = x1 + m1/(m1+m2)**2 * (x2-x1)
+        x_guess = 0.25*x_weighted+0.75*x_mid
+        try:
+            return newton(func=force, x0=x_guess, x1=x_mid, args=(x1, x2, m1, m2, expon), tol=(x2-x1)/100.0 )
+        except:
+            return x_guess
+
+
+def iex_weighted_mid(d, expon=-1.5):
+    x1 = d['iexBidPrice']
+    x2 = d['iexAskPrice']
+    m1 = d['iexBidSize']
+    m2 = d['iexAskSize']
+    if (x1 != 0) and (x2 != 0) and (m1 != 0) and (m2 != 0) and (x2 > x1):
+        return weighted_mid(x1=x1, x2=x2, m1=m1, m2=m2)
+    else:
+        return d['latestPrice']
+
+
+def iex_realtime_prices(tickers, api_key: str, use_free_api=False) -> [float]:
     """
         Asynchronously grab multiple stock prices
     """
     PAID_URL = 'https://cloud.iexapis.com/stable/stock/TICKER/quote?token=YOUR_TOKEN_HERE'
     FREE_URL = 'https://cloud.iexapis.com/stable/tops?token=YOUR_TOKEN_HERE&symbols=TICKER'
     URL = FREE_URL if use_free_api else PAID_URL
-    price_urls =  [ URL.replace('YOUR_TOKEN_HERE',api_key).replace('TICKER',ticker) for ticker in tickers ]
-    data = mgetjson(urls=price_urls)
-    return [ d['iexRealtimePrice'] for d in data ]
+    price_urls = [URL.replace('YOUR_TOKEN_HERE', api_key).replace('TICKER', ticker) for ticker in tickers]
+    d = mgetjson(urls=price_urls)
+    mids = [iex_weighted_mid(di, expon=-1.5) for di in d]
+    return mids
+
+
+if __name__ == '__main__':
+    from microprediction.config_private import IEX_KEY
+
+    print(iex_realtime_prices(tickers=['aapl', 'googl'], api_key=IEX_KEY))
 
 
