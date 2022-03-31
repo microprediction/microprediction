27d5fcece3c67b1bbc10d5dee449bee3823fa03c
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-03-31 10:10:17 -0400

    Update cobego_bobcat.py

--- crawler_skater_examples/cobego_bobcat.py
+++ crawler_skater_examples/cobego_bobcat.py
@@ -16,5 +16,5 @@ if __name__=='__main__':
     skater = RegularFaangStreamSkater(write_key=WRITE_KEY, f=f, use_std=True, max_active=1000)
     skater.set_email('pcotton@intechinvestments.com')
     skater.set_repository(
-        'https://github.com/microprediction/microprediction/blob/master/crawler_skater_examples/cobega_bobcat.py')
+        'https://github.com/microprediction/microprediction/blob/master/crawler_skater_examples/cobego_bobcat.py')
     skater.run()
