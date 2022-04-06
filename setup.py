36ea3dcfa37a8cf7f88e4b0ce7b0d15113ed1c16
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-04-06 09:38:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="microprediction",
-    version="0.23.4",
+    version="1.0.0",
     description="Client for www.microprediction.org turnkey community prediction",
     long_description=README,
     long_description_content_type="text/markdown",
