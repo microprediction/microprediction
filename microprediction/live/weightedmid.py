be5c420c79077c8212a5b4e9596e737a74016a57
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-04-06 10:13:04 -0400

    Update weightedmid.py

--- microprediction/live/weightedmid.py
+++ microprediction/live/weightedmid.py
@@ -1,4 +1,6 @@
-#this belongs somewhere else!
+# This belongs somewhere else!
+# Not used 
+
 from scipy.optimize import newton
 
 def force(x, x1, x2, m1, m2, expon=2):
@@ -16,3 +18,16 @@ def weighted_mid(x1, x2, m1, m2, expon=2):
             return newton(func=force, x0=x_guess, x1=x_mid, args=(x1, x2, m1, m2, expon), tol=(x2-x1)/100.0 )
         except:
             return x_guess
+
+        
+def iex_weighted_mid(d, expon=-1.5):
+    x1 = d['iexBidPrice']
+    x2 = d['iexAskPrice']
+    m1 = d['iexBidSize']
+    m2 = d['iexAskSize']
+    if (x1 != 0) and (x2 != 0) and (m1 != 0) and (m2 != 0) and (x2 > x1):
+        wm = weighted_mid(x1=x1, x2=x2, m1=m1, m2=m2)
+        print({'x1': x1, 'm1': m1, 'x2': x2, 'm2': m2, 'wm': wm})
+        return wm
+    else:
+        return d['latestPrice']
