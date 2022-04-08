d5346d749d55fb692e4d238e47ed4542d1ac8902
Peter Cotton <peter.cotton@microprediction.com>
2022-04-08 13:59:39 -0400

    grains

---
+++ stream_examples_grains/README.md
@@ -0,0 +1,6 @@
+
+## grain streams
+
+Look for 'grain' in the [stream listing](https://www.microprediction.org/browse_streams.html)
+
+The idea is that the Copulas for Wheat, Corn and Soyb might be rather interesting
--- stream_examples_grains/grains.py
+++ stream_examples_grains/grains.py
@@ -1,4 +1,4 @@
-from microprediction.config_private import LEGLESS_OCELOT as WRITE_KEY
+from microprediction.config_private import ZOOBLAST_MOOSE as WRITE_KEY
 from microprediction.polling import MultiChangePoll
 from microprediction.live.grains import GRAIN_NAMES, iex_scaled_log_grains
 
@@ -14,7 +14,7 @@ def func()->[float]:
 
 
 if __name__=='__main__':
-    mcp = MultiChangePoll(write_key=WRITE_KEY, names = GRAIN_NAMES, interval=5, func=func, with_copulas=False)
+    mcp = MultiChangePoll(write_key=WRITE_KEY, names = GRAIN_NAMES, interval=5, func=func, with_copulas=True)
     mcp.set_repository('https://github.com/microprediction/microprediction/tree/master/stream_examples_grains')
     mcp.run()
 
