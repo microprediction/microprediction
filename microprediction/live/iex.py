447d7583274901ec9ebd45b4699ac4ad35d621b7
Peter Cotton <peter.cotton@microprediction.com>
2022-04-08 15:44:00 -0400

    stuff

--- microprediction/live/iex.py
+++ microprediction/live/iex.py
@@ -7,13 +7,20 @@ from getjson import getjson
 def iex_latest_prices(tickers, api_key:str)->[float]:
     """
         Grab multiple stock prices
+        IEX allows only 100 at a time
     """
-    symbols = ','.join(tickers)
-    paid_url = 'https://cloud.iexapis.com/v1/stock/market/batch?types=price&symbols='+symbols+'&token=' + api_key
-    print(paid_url)
-    data = getjson(paid_url)
-    mids = [ data[ticker.upper()]['price'] for ticker in tickers]
-    return mids
+    if len(tickers)>100:
+        tickerlists = [tickers[i:i + 100] for i in range(0, len(tickers), 100)]
+        mids = [ iex_latest_prices(tickers=ticks, api_key=api_key) for ticks in tickerlists ]
+        mids_flat = [item for sublist in mids for item in sublist]
+        return mids_flat
+    else:
+        symbols = ','.join(tickers)
+        paid_url = 'https://cloud.iexapis.com/v1/stock/market/batch?types=price&symbols='+symbols+'&token=' + api_key
+        print(paid_url)
+        data = getjson(paid_url)
+        mids = [ data[ticker.upper()]['price'] for ticker in tickers]
+        return mids
 
 
 if __name__ == '__main__':
