9c9e44b9660d20862bad736fb00e14cd5247af33
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-09-27 09:05:28 -0400

    Update run_default_crawler_forever.sh

--- shell_examples/run_default_crawler_forever.sh
+++ shell_examples/run_default_crawler_forever.sh
@@ -1,4 +1,3 @@
-set -e
 echo "This script will run DefaultCrawler from a new virtual env"
 echo "If it fails you might need to brew install python"
 mkdir -p crawling_working_dir
@@ -13,7 +12,6 @@ echo "------ Installation complete ------"
 echo "Next, burning a write_key which will be your identity. This may take a long time."
 sleep 3
 
-# Make a write key
 python3 -c "from microprediction import new_key;WRITE_KEY = new_key();print(WRITE_KEY)" > "WRITE_KEY.txt"
 
 sleep 3 
@@ -26,6 +24,7 @@ while [ $(( $(date +%s) - 30000000 )) -lt $START ]; do
     write_key=$(cat "WRITE_KEY.txt")
     pip install --upgrade microprediction.git
     python3 -c "from microprediction import DefaultCrawler;crawler = DefaultCrawler(write_key='"$write_key"'); crawler.run()"
+    echo "I guess something went wrong, or this is a scheduled update time. Not to worry either way. We shall resume in 60 seconds". 
     sleep 60
 done
 
