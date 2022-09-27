b92cf6afee53903b6db58b078cb4d34ed3620542
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-09-27 09:45:18 -0400

    Update run_default_crawler_forever.sh

--- shell_examples/run_default_crawler_forever.sh
+++ shell_examples/run_default_crawler_forever.sh
@@ -29,7 +29,7 @@ echo $write_key
 START=`date +%s` 
 while [ $(( $(date +%s) - 300000 )) -lt $START ]; do
     write_key=$(cat "WRITE_KEY.txt")
-    pip install --upgrade microprediction.git
+    pip install --upgrade git+https://github.com/microprediction/microprediction.git
     python3 -c "from microprediction import DefaultCrawler;crawler = DefaultCrawler(write_key='"$write_key"'); crawler.run()"
     echo "I guess something went wrong, or this is a scheduled update time. Not to worry either way. We shall resume in 60 seconds". 
     sleep 60
