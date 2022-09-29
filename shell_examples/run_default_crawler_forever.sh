1a842fc08298835396efad1d2e87b2727ce99518
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-09-29 07:27:41 -0400

    Update run_default_crawler_forever.sh

--- shell_examples/run_default_crawler_forever.sh
+++ shell_examples/run_default_crawler_forever.sh
@@ -17,7 +17,16 @@ then
    echo "Found "$WRITE_KEY_FILE
 else
    echo "Next, burning a write_key which will be your identity. This may take a long time. Go get lunch."
-   echo 
+   sleep 10
+   echo "Write keys are explained at https://www.microprediction.com/private-keys"
+   sleep 10
+   echo "Or read the docs https://microprediction.github.io/microprediction/" 
+   sleep 10
+   echo "Or listen to the first chapter of the book https://github.com/microprediction/building_an_open_ai_network/blob/main/docs/assets/audio/Microprediction_Chapter_1.mp3"
+   sleep 10
+   echo "Or read our blog https://medium.com/@microprediction" 
+   sleep 10
+   echo "Or view the video tutorials https://microprediction.github.io/microprediction/videos.html"
    sleep 3
    #python3 -c "import os;os.environ['MUID_VERBOSITY']='1';from microprediction import new_key;WRITE_KEY = new_key(difficulty=11);fh = open('WRITE_KEY.txt', 'w');fn.write(WRITE_KEY);fn.close()"
    python3 -c "from microprediction import new_key;WRITE_KEY = new_key(difficulty=11);print(WRITE_KEY)" > WRITE_KEY.txt
