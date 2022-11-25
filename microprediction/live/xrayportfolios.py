1238c435a6b260213765621564cc920235b5a1b8
Peter Cotton <peter.cotton@microprediction.com>
2022-11-25 09:42:16 -0500

    portfolios

--- microprediction/live/xrayportfolios.py
+++ microprediction/live/xrayportfolios.py
@@ -6,12 +6,17 @@ from getjson import getjson
 from microprediction.whereami import TOP
 import os
 import json
+import math
 
-
-NUM_PORTFOLIOS = 80
+TILT = 0.1
+TILE_NDX = [-3,-2,-1,0,1,2,3]
+NUM_SIZE_PORTFOLIOS = len(TILE_NDX)
+NUM_PORTFOLIOS = 20
 XRAY_PORTFOLIO_NAMES = ['xray_' + str(i)+'.json' for i in range(NUM_PORTFOLIOS)]
+XRAY_COMMON_JSON = os.path.join(TOP, 'live', 'xrayportfolios.json')
 
 from microprediction.live.xraytickers import STOCK_THRESHOLD
+
 
 def normalize(w):
     return [ wi/sum(w) for wi in w]
@@ -24,13 +29,19 @@ def create_xray_portfolios():
     tickers = get_xray_tickers()
     prices = iex_latest_prices(tickers=tickers, api_key=IEX_KEY)
     common = iex_common_stock_with_balance_sheet_tickers(api_key=IEX_KEY, tickers=tickers, return_tickers=False, threshold=STOCK_THRESHOLD)
-    index_like = normalize([price * cm for price, cm in zip(prices, common)])
-    XRAY_PORTFOLIOS = [normalize([wi * math.exp(np.random.randn()) for wi in index_like]) for _ in
-                       range(NUM_PORTFOLIOS)]
-    XRAY_PORTFOLIOS_DICT = dict([(k, p) for k, p in enumerate(XRAY_PORTFOLIOS)])
-    XRAY_COMMON_JSON = os.path.join(TOP, 'live', 'xrayportfolios.json')
+    norm_common = normalize(common)
+    norm_cap = normalize([price * cm for price, cm in zip(prices, norm_common)])
+    xray_portfolios = list()
+    the_one_over_n = normalize([1.0 for nc in norm_cap])
+    xray_portfolios.append(the_one_over_n)
+    for ndx in TILE_NDX:
+        cap_tilt = normalize([math.exp(nc*TILT*ndx) for nc in norm_cap])
+        xray_portfolios.append(cap_tilt)
+
+    xray_portfolios_dict = dict([(k, p) for k, p in enumerate(xray_portfolios)])
+
     with open(XRAY_COMMON_JSON, 'wt') as fp:
-        json.dump(obj=XRAY_PORTFOLIOS_DICT, fp=fp)
+        json.dump(obj=xray_portfolios_dict, fp=fp)
 
 
 def get_xray_portfolios():
