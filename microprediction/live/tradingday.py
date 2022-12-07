122f3d0c448ec37a79135da971cffa4f165c1279
Peter Cotton <peter.cotton@microprediction.com>
2022-12-07 15:31:31 -0500

    tiny change to yarx_moving

--- microprediction/live/tradingday.py
+++ microprediction/live/tradingday.py
@@ -1,4 +1,4 @@
-
+import datetime
 
 def yarx_moving() -> bool :
     """
@@ -8,9 +8,23 @@ def yarx_moving() -> bool :
     from microprediction import MicroReader
     mr = MicroReader()
     lagged_values, lagged_times = mr.get_lagged_values_and_times(name='quick_yarx_aapl.json')
-    return (time.time()-lagged_times[0])<3*60*60
+    return (time.time()-lagged_times[0])<10*60
+
+
+def eastern():
+    # Get the current time in Eastern Time
+    eastern_time = datetime.datetime.now(datetime.timezone.utc).astimezone(datetime.timezone(-datetime.timedelta(hours=5)))
+
+    # Format the time as a string in Snowflake-friendly format
+    return eastern_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+
+
+
+
 
 
 
 if __name__=='__main__':
     print({'yarx_moving':yarx_moving()})
+    print(eastern())
