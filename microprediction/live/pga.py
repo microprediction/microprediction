77679c49767f0c387aaf0b91db4a6206e5060f82
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2023-04-19 12:58:33 -0400

    Create pga.py

---
+++ microprediction/live/pga.py
@@ -0,0 +1,19 @@
+
+from getjson import getjson
+
+def pga_contenders():
+    return getjson('https://micropredictionmiscstreams.pythonanywhere.com/contenders')
+  
+  
+def contender_index(player_name, contenders=None):
+    if contenders is None:
+      contenders = pga_contenders()
+    matches = [ ndx for ndx,name in contenders.items() if name==player_name ]
+    if matches:
+        return matches[0]
+    else:
+        return -1
+
+
+def contender_stream_name(contender_ndx, hole):
+    return 'pga_contender_'+str(contender_ndx)+'_hole_'+str(hole)+'.json'
