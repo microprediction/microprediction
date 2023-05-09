5665ca908a2eeb630e332444475499282bcd59b5
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2023-05-09 04:55:21 -1000

    Update aflat_sloth.py

--- crawler_examples/aflat_sloth.py
+++ crawler_examples/aflat_sloth.py
@@ -3,15 +3,14 @@ from tdigest import TDigest
 from microprediction import SequentialStreamCrawler, DistMachine
 
 # Crawler that goes after sector streams
+# Illustration of inserting a DistMachine into a SequentialStreamCrawler
+
 
 class DigestMachine(DistMachine):
 
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         self.digest = TDigest()
-
-    def include_stream(self, name=None, **ignore):
-        return 'rdps' in name
 
     def update(self, value, dt=None, **kwargs):
         self.digest.update(value)
@@ -20,8 +19,17 @@ class DigestMachine(DistMachine):
         return self.digest.percentile(100. * p)
 
 
+class SequentialSectorCrawler(SequentialStreamCrawler):
+
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+
+    def include_stream(self,name):
+        return 'rdps' in name
+
+
 if __name__ == "__main__":
-    crawler = SequentialStreamCrawler(write_key=ALFLAT_SLOTH, min_lags=500, machine_type=DigestMachine, max_active=200)
+    crawler = SequentialSectorCrawler(write_key=ALFLAT_SLOTH, min_lags=500, machine_type=DigestMachine, max_active=200)
     crawler.set_repository(
         url='https://github.com/microprediction/microprediction/blob/master/crawler_examples/aflat_sloth.py')
     crawler.min_lags = 500
